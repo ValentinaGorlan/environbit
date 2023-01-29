3de23de9822513d716df4df7d3bc469a7db6faee
ValentinaGorlan <valentina.gorlanova.ru@gmail.com>
2023-01-29 14:48:46 +0300

    fix 1 problem

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -10,7 +10,7 @@ const MovieList = ({ handleSelect, sortProp, initialValue, setRef, isReset}) => 
 
   const NotFound = () => {
     return (
-      <div className={s.container} ref={ref}>
+      <div className={s.notFound} ref={ref}>
           <h1>Nada foi encontrado de acordo com sua solicitação.</h1>
           <h2>Atualize seus filtros.</h2>
       </div>
@@ -77,26 +77,29 @@ const MovieList = ({ handleSelect, sortProp, initialValue, setRef, isReset}) => 
     });
 
     return (
-      <ul className={s.flex}>
-          {moviesList.length === 0 ? NotFound() : null}
-        {elements}
-      </ul>)
+      <div>
+        {moviesList.length === 0 ? NotFound() : null}
+        <ul className={s.flex}>
+          {elements}
+        </ul>
+      </div>
+    )
   };
 
   const sortMovies = () => {
     let valuesArr= data.map(item => [item.year, ...item.country, ...item.genre].filter(value => sortParamss.indexOf(value) > -1));
 
-    let indArr1 = valuesArr.map((item, ind) => item.length === 3 ? ind : null).filter(ind => ind);
-    let indArr2 = valuesArr.map((item, ind) => item.length === 2 ? ind : null).filter(ind => ind);
-    let indArr3 = valuesArr.map((item, ind) => item.length === 1 ? ind : null).filter(ind => ind);
+    let indArr1 = valuesArr.map((item, ind) => item.length === 3 ? ind : null).filter(ind => ind !== null);
+    let indArr2 = valuesArr.map((item, ind) => item.length === 2 ? ind : null).filter(ind => ind !== null);
+    let indArr3 = valuesArr.map((item, ind) => item.length === 1 ? ind : null).filter(ind => ind !== null);
 
     let result1 = data.map((item, ind) => indArr1.indexOf(ind) > -1 ? item : null).filter(item => item);
     let result2 = data.map((item, ind) => indArr2.indexOf(ind) > -1 ? item : null).filter(item => item)
-    let result3 = data.map((item, ind) => indArr3.indexOf(ind) > -1 ? item : null).filter(item => item)
+    let result3 = data.map((item, ind) => indArr3.indexOf(ind) > -1 ? item : null).filter(item => item);
 
     if (sortParamss.filter(s => s).length === 0) {
       return data
-    } else if (result1.length === 0 && sortParamss.filter(s => s).length === 3) {
+    } else if (result1.length === 0 && sortParamss.filter(s => s).length === 3 || result2.length === 0 && sortParamss.filter(s => s).length === 2) {
       return [];
     } else if (result1.length > 0 && sortParamss.filter(s => s).length === 3) {
       return result1
